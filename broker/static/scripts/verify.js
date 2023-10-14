441db493edf294e7ceea028fa0234253ad14e29b
Marcus Daniel <momsdboy@gmail.com>
2023-10-14 01:10:13 +0100

    async email verification creation

--- broker/static/scripts/verify.js
+++ broker/static/scripts/verify.js
@@ -4,30 +4,29 @@
     const verifyButton = document.getElementById('verify-button');
     const errorMessage = document.getElementById('error-message');
 
-    verifyForm.addEventListener('submit', function (event) {
-      event.preventDefault(); // Prevent the form from submitting normally
 
-      // Gather form data
-      const formData = new FormData(verifyForm);
+    verifyForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        
+        const formData = new FormData(verifyForm);
+        
+        const xhr = new XMLHttpRequest();
 
-      // Use the Fetch API to send a POST request to your server
-      fetch(verifyEmailEndpoint, {
-        method: 'POST',
-        body: formData,
-      })
-        .then((response) => response.json()) // Assuming your server returns JSON
-        .then((data) => {
-          if (data.success) {
-            // Redirect to the dashboard on success
-            window.location.href = '/dashboard';
-          } else {
-            // Display the error message
-            errorMessage.textContent = data.message;
-          }
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+        xhr.open('POST', '/verify_email/', true);
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                if (response.success) {
+                    window.location.href = '/dashboard'; 
+                } else {
+                    document.getElementById('error-message').textContent = response.message; 
+                }
+            } else {
+                // Handle other HTTP status codes if needed
+            }
+        };
+
+        xhr.send(formData);
     });
   });
-
